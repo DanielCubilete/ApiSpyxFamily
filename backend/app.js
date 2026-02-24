8417cb4563abf771555b9b7927d2ff1eef10ab60
DanielCubilete <daniel.dominguezcorrea@cieep.com>
2026-02-24 13:57:28 +0100

    Force CORS headers for all responses (Vercel fix)

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,17 @@ const corsOptions = {
 
 // Middlewares
 app.use(cors(corsOptions));
+// Forzar header CORS manualmente para todas las respuestas
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
