2f0c3c2230d33d3cc71c5a8bb4423849262e0754
DanielCubilete <daniel.dominguezcorrea@cieep.com>
2026-02-27 00:07:25 +0100

    AABB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const URI = process.env.MONGODB_URI || 'mongodb+srv://admin:vgZILaxgmRpOwzt9@cluster0.ye1az4p.mongodb.net/Spyxfamily?appName=Cluster0';
+const URI = process.env.MONGODB_URI || 'mongodb+srv://admin:vgZILaxgmRpOwzt9@cluster0.ye1az4p.mongodb.net/spyxfamily?appName=Cluster0';
 mongoose.connect(URI)
     .then(() => console.log('DB is connected'))
     .catch(err => console.error(err));
