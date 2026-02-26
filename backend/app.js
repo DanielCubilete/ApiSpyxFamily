0fe7a5b972fd132bca97c67f10e7f0beff3644d2
DanielCubilete <daniel.dominguezcorrea@cieep.com>
2026-02-27 00:19:30 +0100

    AAADD

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ const allowedOrigins = [
     'http://localhost:4200',
     'https://api-spyx-family-odt6wb5zp-daniels-projects-83665ae7.vercel.app',
     'https://api-spyx-family-app.vercel.app',
+    'https://api-spyx-family-lsawxnk8q-daniels-projects-83665ae7.vercel.app', // Dominio frontend actual
     /^https:\/\/.*\.vercel\.app$/ // Todos los subdominios de Vercel
 ];
 
